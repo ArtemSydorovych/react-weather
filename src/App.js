1f27ef1a9cf0c5b5ca49fab8de687ba345a1f083
ArtemSydorovych <artem.sydorovych@gmail.com>
2022-06-03 20:45:57 +0200

    innit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+import "./styles/weather.css"
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [weather, setWeather] = useState('');
+  const [city, setCity] = useState('');
+  const apiKey = process.env.REACT_APP_APIKEY;
+
+  const apiCall = async (e) => {
+    e.preventDefault()
+    const loc = e.target.elements.loc.value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apiKey}`;
+    const req = axios.get(url);
+    const res = await req;
+    setWeather({
+      descp: res.data.weather[0].description,
+      temp: res.data.main.temp,
+      city: res.data.name,
+      humidity: res.data.main.humidity,
+      press: res.data.main.pressure,
+    })
+
+    setCity(res.data.name)
+
+  }
+
+  //Converting K to C
+  let k = weather.temp;
+  let C = k - 273.15
+
+  const Weath = () => {
+    return <div>
+      <div className="winfo">
+        Weather information for {city}
+        <hr></hr>
+      </div>
+      <div className="Weath">
+        <div className="welement">
+          Weather : {weather.descp}
+        </div>
+        <div className="welement">
+          Temperature : {C.toFixed(2)} &#8451;
+        </div>
+        <div className="welement">
+          Humidity :{weather.humidity} %
+        </div>
+        <div className="welement">
+          Pressure :  {weather.press} mb
+        </div>
+      </div>
     </div>
-  );
+  }
+  return (<>
+        <div className="weathhead">Weather Info</div>
+        <div className="mainweather">
+          <div className="weather">
+            <form onSubmit={apiCall} className="form">
+              <input type="text"
+                     placeholder="city"
+                     name="loc" />
+              <button className="bttn">Search</button>
+            </form>
+
+            {weather && <Weath />}
+          </div>
+        </div>
+      </>
+  )
 }
 
-export default App;
+export default App
